1a8062f5619e7949f1951791b49875eb80cb2f21
Pattan Fakhruddin <patfakhr@publicisgroupe.net>
2018-08-02 13:39:31 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const m = process.env.MODULE;
+
+module.exports = {
+  entry: `./src/js/index.${m}.js`,
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'], exclude: /node_modules/ },
+      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'], include: /node_modules\/bootstrap/ },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `./src/html/index.${m}.html`,
+      inject: 'body'
+    })
+  ]
+};
