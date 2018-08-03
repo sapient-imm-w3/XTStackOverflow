b5ea41e1ad5f6f53ce163692f926fa4b7d3b5316
Srikar <ms_ece@hotmail.com>
2018-08-04 01:09:26 +0530

    Table Applied for Questions

--- src/js/Admin/index.Admin.js
+++ src/js/Admin/index.Admin.js
@@ -1,10 +1,25 @@
 import '../../vendors/DataTables/css/jquery.dataTables.min.css';
 import 'bootstrap/scss/bootstrap.scss';
 import '../../css/index.css';
+import 'jquery';
+import $ from 'jquery';
 import '../../vendors/DataTables/js/jquery.dataTables.min.js';
 import '../Admin/views/admin.view';
 import '../Admin/controllers/admin.controller';
 
-
+$(document).ready(function() {
+    $('#example').DataTable( {
+        columnDefs: [ {
+            orderable: false,
+            className: 'select-checkbox',
+            targets:   0
+        } ],
+        select: {
+            style:    'os',
+            selector: 'td:first-child'
+        },
+        order: [[ 1, 'asc' ]]
+    } );
+} );
 
 
--- src/js/Admin/views/admin.view.js
+++ src/js/Admin/views/admin.view.js
@@ -1,32 +1,40 @@
-import $ from 'jQuery';
 import { revokeFlaggedQuestion } from '../services/admin.service';
 export function createHTMLElement(html) {
     const template = document.createElement('template');
     template.innerHTML = html;
     return template.content.firstElementChild;
 }
+
+export function layout(){
+    const table = createHTMLElement(`
+    <table id="example" class="display" style="width:100%">
+    <thead>
+        <tr>
+            <th>Question</th>
+            <th>FlagCount</th>
+            <th>Revoke</th>
+        </tr>
+    </thead>
+    <tbody id="tableBody">
+    </tbody>
+</table>`);
+document.getElementById("flagged_questions").appendChild(table);
+}
+
 export function createFlaggedDiv(question) {
     const flaggedDiv = createHTMLElement(
-        `
-        <div id="" class="flaggedquestion">
-        <div class="row">
-        <div class="col-md-3 row text-muted text-center flagCount">
-        <div class="col-md-9">${question.flag_count}
-        <br> flags
-        </div>
-        </div>
-        <div class="col-md-6">
-        <a href="#">${question.text}</a>
-        </div>
-        <button type="button" class="btn btn-warning" id="${question.id}">Revoke Flag</button>
-        </div>
-        </div> 
-    `
-    )
-    document.getElementById("flagged_questions").appendChild(flaggedDiv);
-    document.getElementById(`${question.id}`).onclick = () =>{
+            `<tr>
+                <td>${question.text}</td>
+                <td>${question.flag_count}</td>
+                <td>
+                <button type="button" class="btn btn-warning" id ="${question.id}">Revoke Flag</button>
+                </td>
+            </tr>
+            `)
+    document.getElementById("tableBody").appendChild(flaggedDiv);
+    document.getElementById(`${question.id}`).onclick = () => {
         revokeFlaggedQuestion(`${question.id}`);
     }
-    
-} 
+
+}
 
