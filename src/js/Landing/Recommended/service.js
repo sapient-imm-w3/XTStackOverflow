3b41489a5c89fa2f09cbd75a7258d23f71cf4077
Sathi Reddy <satreddy@publicisgroupe.net>
2018-08-03 18:13:33 +0530

    multiple categories in recommended

--- src/js/Landing/Recommended/service.js
+++ src/js/Landing/Recommended/service.js
@@ -6,24 +6,21 @@ export function getRecommended() {
   fetch(getUrl)
   .then((resp) => resp.json())
   .then((data) => {
-    let user_category = data.fav_categories[0];
-    console.log('inside recommended');
-    console.log(user_category);
-    let get1Url = `http://localhost:3000/questions?is_flagged=false`;
+    data.fav_categories.map(function(cat){
+      let get1Url = `http://localhost:3000/questions?is_flagged=false`;
     document.getElementById('recommended').innerHTML = "";
-
     fetch(get1Url)
     .then((resp) => resp.json())
     .then((data) => {
     let rec = data;
     console.log(rec);
-    let title = getTitle(user_category);
+    let title = getTitle(cat);
     document.getElementById('recommended').appendChild(title);
     let count = 0;
     return rec.map((rec) => {
       if(count<5){
       for(let i=0; i< rec.categories.length; i++){
-        if(rec.categories[i].name==user_category){
+        if(rec.categories[i].name==cat){
             let display = getRecQuestion(rec.text);
             document.getElementById('recommended').appendChild(display);
             count++;
@@ -33,5 +30,7 @@ export function getRecommended() {
         }
       })
     })
+    })
+    
   })
 }
--- src/js/Landing/Recommended/view.js
+++ src/js/Landing/Recommended/view.js
@@ -8,7 +8,7 @@ export function getTitle(category) {
     let display = createHTMLElement(`
       <!-- Card -->
       <div  style="border:5px; background: white">
-      <p style="margin-top: 0.5rem; margin-bottom: 0.5rem;">${question}</p>
+      <p style="margin: 0.5rem">${question}</p>
       </div>
       `);
     return display;
