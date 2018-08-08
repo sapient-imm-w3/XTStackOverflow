22eedbebd679fabf179604b0ee4a0f4c7c113616
tejeswarsahu@gmail.com <tejeswarsahu@gmail.com>
2018-08-08 15:49:30 +0530

    just check in it

--- src/js/Admin/AdminApp/controller/adminapp.controller.js
+++ src/js/Admin/AdminApp/controller/adminapp.controller.js
@@ -1,5 +1,5 @@
 import $ from '../../../../../node_modules/jquery/dist/jquery'
-import {renderWholeCategoryView,createTopic,Topic,Category,createCategory,getAllCategories} from '../services/adminapp.service'
+import {isCategoryAlreadyExist,getAllCatFromFirebase,insertCategoryToFirebase,renderWholeCategoryView,createTopic,Topic,Category,createCategory,getAllCategories} from '../services/adminapp.service'
 import {renderCategoryView} from '../views/adminapp.view'
 
 
@@ -8,18 +8,28 @@ $('#listModal').on('click', '#listSave', function (event) {
 
     let catName = $('#formGroupListTitleInput').val();
     console.log("category : "+catName+" going to be added ");
+    $('#listClose').click();
+    isCategoryAlreadyExist(catName).then(function(isCategoryExist){
+  if(isCategoryExist){
+      alert("Category with name "+ catName + "already exist.Please try with a different name.");
+    console.log("just returning the control");
+    //return;
+  }
+  else{
     let singleCatObj = new Category(catName);
-    createCategory(singleCatObj).then(data1 =>{ 
-        getAllCategories().then(data => {
+    insertCategoryToFirebase(singleCatObj).then(data1 =>{ 
+        getAllCatFromFirebase().then(data => {
         console.log(data);
         renderCategoryView(data);
     })
+    
 });
-   
-   $('#listClose').click();
-
+  }
   
-  })
+    })//end of then callback function
+   //$('#listClose').click();
+ 
+  });//end of onlick event
 //////////////////   TOPIC RELATED      ////////////////////////
 export let addTopic = function(event){
     console.log("Add Topic button got clicked");
